9fed4bae75693ea833b86f7c17064af0794e77f6
Nikeeta26 <nikeetakaudare14@gmail.com>
2024-05-28 16:31:29 +0530

    change

--- Project_airBnB/index.js
+++ Project_airBnB/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("/models/listing.js");
 
 const MONGODB_URL = 'mongodb://127.0.0.1:27017/wanderlust';
 main().then(()=>{
@@ -16,8 +17,23 @@ app.get("/",(req,res)=>{
     res.send("hello");
 });
 
+app.get("/testListing",async(req,res)=>{
+   let sampleListing = new Listing({
+      title:"my New village",
+      description:"By the beach",
+      price:12000,
+      location:"pune",
+      country : "india",
+   });
+
+   await sampleListing.save();
+   console.log("sample was save");
+   res.send("success testing");
+});
+
 let port = 8080;
 app.listen(port,()=>{
     console.log("server run on port 8080");
 });
+
 
--- Project_airBnB/models/listing.js
+++ Project_airBnB/models/listing.js
@@ -1,4 +1,31 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema({
+const Schema = mongoose.Schema;
 
+const ListingSchema = new Schema ({
+  title : {
+   type: String,
+    require :true
+  },
+  description : {
+  type:  String,
+
+  },
+  image :{
+     type: String,
+     set:(v)=> v === " "
+     ? "https://images.unsplash.com/photo-1716872491089-a43035b4db58?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHx8" : v,
+  },
+  price :{
+    type: Number,
+  },
+  location :{
+    type: String,
+  },
+  country : {
+    type:String,
+  }
 });
+
+const Listing = mongoose.model("Listing", ListingSchema);
+
+module.export = Listing;
