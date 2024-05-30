d771c79b80950b56995653f4568bd096f367b9aa
Nikeeta26 <nikeetakaudare14@gmail.com>
2024-05-30 14:50:47 +0530

    add ejs-mate

--- Project_airBnB/index.js
+++ Project_airBnB/index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
-
+const ejsMate = require("ejs-mate");
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
@@ -23,6 +23,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
 
 app.get("/", (req, res) => {
   res.send("Hi, I am root");
@@ -58,6 +59,16 @@ app.put("/listings/:id", async (req, res) => {
   let { id } = req.params;
   await Listing.findByIdAndUpdate(id, { ...req.body.listing });
   res.redirect(`/listings/${id}`);
+});
+
+
+//delete Route
+app.delete("/listings/:id",async(req,res)=>{
+   let{ id } = req.params;
+   const deletedListing = await Listing.findByIdAndDelete(id);
+   console.log(deletedListing);
+   res.redirect("/listings");
+
 });
 
 //Show Route
