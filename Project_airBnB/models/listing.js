4c196863000fc5113b6547b67cf80e490ffd832d
Nikeeta26 <nikeetakaudare14@gmail.com>
2024-06-01 12:35:24 +0530

    change

--- Project_airBnB/models/listing.js
+++ Project_airBnB/models/listing.js
@@ -1,32 +1,57 @@
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+
+// const listingSchema = new Schema({
+//   title: {
+//     type: String,
+//     required: true,
+//   },
+//   description: String,
+//   image: {
+//     type: String,
+//     filename: String,
+//     url: String,
+//     default:
+//       "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+//     set: (v) =>
+//       v === ""
+//         ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+//         : v,
+//   },
+//   price: Number,
+//   location: String,
+//   country: String,
+// });
+
+// const Listing = mongoose.model("Listing", listingSchema);
+// module.exports = Listing;
+
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const ListingSchema = new Schema ({
-  title : {
-   type: String,
-    require :true
-  },
-  description : {
-  type:  String,
-
-  },
-  image :{
-     type: String,
-     default: "https://images.unsplash.com/photo-1716872491089-a43035b4db58?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHx8",
-     set:(v)=> v === " "
-     ? "https://images.unsplash.com/photo-1716872491089-a43035b4db58?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHx8" : v,
-  },
-  price :{
-    type: Number,
-  },
-  location :{
+const listingSchema = new Schema({
+  title: {
     type: String,
+    required: true
   },
-  country : {
-    type:String,
-  }
+  description: String,
+  image: {
+    type: String,
+    set: function(value) {
+      // If the value is an object (like when it's set by Mongoose internally),
+      // extract the 'url' property; otherwise, use the value directly
+      return typeof value === "object" ? value.url : value;
+    },
+    default:
+      "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+  },
+  price: {
+    type: Number,
+    default: 0
+  },
+  location: String,
+  country: String
 });
 
-const Listing = mongoose.model("Listing", ListingSchema);
-
+const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
