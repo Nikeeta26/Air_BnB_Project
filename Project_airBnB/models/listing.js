2bb1801a2448aa18292b95a73f0fb4580298fc7d
Nikeeta26 <nikeetakaudare14@gmail.com>
2024-06-04 15:34:44 +0530

    change set function

--- Project_airBnB/models/listing.js
+++ Project_airBnB/models/listing.js
@@ -35,15 +35,19 @@ const listingSchema = new Schema({
     required: true
   },
   description: String,
-  image: {
-    type: String,
-    set: function(value) {
-      // If the value is an object (like when it's set by Mongoose internally),
-      // extract the 'url' property; otherwise, use the value directly
-      return typeof value === "object" ? value.url : value;
-    },
-    default:
-      "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+  image :{
+    //  type: String,
+    type:String,
+     default:"https://images.unsplash.com/photo-1716872491089-a43035b4db58?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHx8",
+    //  set:(v)=> v === " "
+    //  ?"https://images.unsplash.com/photo-1570129477492-45c003edd2be?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
+
+    set: function(v) {
+      if (!v || v.trim() === "") {
+        return "https://images.unsplash.com/photo-1570129477492-45c003edd2be?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+      }
+      return v;
+    }
   },
   price: {
     type: Number,
